2e1c58d798f02375342c22b7f52c3905ce419e34
habiburRhaman05 <devhabib2005@gmail.com>
2026-01-11 20:13:10 +0600

    update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,10 @@ import { NextResponse } from 'next/server';
 
 
 export default async function middleware(request: NextRequest) {
-// Better-Auth এর ডিফল্ট সেশন কুকি চেক
+
   const sessionToken = request.cookies.get("better-auth.session_token") || 
                        request.cookies.get("__better_auth_session"); // Production এ এই নাম হতে পারে
-console.log(sessionToken);
+
 
   const { pathname } = request.nextUrl;
   
